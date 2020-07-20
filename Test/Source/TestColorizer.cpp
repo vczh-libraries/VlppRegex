b61e26da85929f8d1cd6c67dca1c15f5daf53fe3
Zihan Chen <vczh@163.com>
2020-07-20 06:26:46 -0700

    Update TestColorizer.cpp

--- Test/Source/TestColorizer.cpp
+++ Test/Source/TestColorizer.cpp
@@ -2,7 +2,7 @@
 
 TEST_FILE
 {
-	TEST_CASE(L"Test RegexLexerColorizer")
+	TEST_CASE(L"Test RegexLexerColorizer 1")
 	{
 		List<WString> codes;
 		codes.Add(L"/d+(./d+)?");
@@ -26,6 +26,29 @@ TEST_FILE
 		{
 			const wchar_t input[] = L"b\"\"genius\"";
 			vint expect[] = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+			TEST_ASSERT(AssertColorizer(colors, expect, colorizer, input, true) == nullptr);
+		}
+	});
+
+	TEST_CASE(L"Test RegexLexerColorizer 2")
+	{
+		List<WString> codes;
+		codes.Add(L"/w+");
+		codes.Add(L"\"[^\"]*\"");
+		codes.Add(L"/s+");
+
+		vint colors[100];
+		RegexProc proc;
+		proc.colorizeProc = &ColorizerProc;
+		proc.argument = colors;
+		RegexLexer lexer(codes, proc);
+		RegexLexerColorizer colorizer = lexer.Colorize();
+
+		colorizer.Pass(L'\r');
+		colorizer.Pass(L'\n');
+		{
+			const wchar_t input[] = L"\"text\"";
+			vint expect[] = { 1, 1, 1, 1, 1, 1 };
 			TEST_ASSERT(AssertColorizer(colors, expect, colorizer, input, true) == nullptr);
 		}
 	});
