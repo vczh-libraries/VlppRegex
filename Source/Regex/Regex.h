bd26c94d68635ba20feeecd0bffff92068a10678
vczh <vczh@outlook.com>
2021-10-23 23:17:21 -0700

    Update release

--- Source/Regex/Regex.h
+++ Source/Regex/Regex.h
@@ -86,7 +86,7 @@ Data Structure
 			/// {
 			///     Regex regex(L"^/.*?((?C/S+)(/.*?))+$");
 			///     auto match = regex.MatchHead(L"C++ and C# are my favorite programing languages");
-			///     FOREACH(RegexString, capture, match->Captures())
+			///     for (auto capture : match->Captures())
 			///     {
 			///         Console::WriteLine(capture.Value());
 			///     }
@@ -100,7 +100,7 @@ Data Structure
 			/// {
 			///     Regex regex(L"^/.*?((<lang>C/S+)(/.*?))+$");
 			///     auto match = regex.MatchHead(L"C++ and C# are my favorite programing languages");
-			///     FOREACH(RegexString, capture, match->Groups().Get(L"lang"))
+			///     for (auto capture : match->Groups().Get(L"lang"))
 			///     {
 			///         Console::WriteLine(capture.Value());
 			///     }
@@ -270,7 +270,7 @@ Regex
 			///     Regex regex(L"C/S+");
 			///     RegexMatch::List matches;
 			///     regex.Search(L"C++ and C# are my favorite programing languages", matches);
-			///     FOREACH(Ptr<RegexMatch>, match, matches)
+			///     for (auto match : matches)
 			///     {
 			///         Console::WriteLine(match->Result().Value());
 			///     }
@@ -287,7 +287,7 @@ Regex
 			///     Regex regex(L"C/S+");
 			///     RegexMatch::List matches;
 			///     regex.Split(L"C++ and C# are my favorite programing languages", false, matches);
-			///     FOREACH(Ptr<RegexMatch>, match, matches)
+			///     for (auto match : matches)
 			///     {
 			///         Console::WriteLine(match->Result().Value());
 			///     }
@@ -304,7 +304,7 @@ Regex
 			///     Regex regex(L"C/S+");
 			///     RegexMatch::List matches;
 			///     regex.Cut(L"C++ and C# are my favorite programing languages", false, matches);
-			///     FOREACH(Ptr<RegexMatch>, match, matches)
+			///     for (auto match : matches)
 			///     {
 			///         Console::WriteLine(match->Result().Value());
 			///     }
@@ -559,7 +559,7 @@ Tokenizer
 			///     RegexLexerColorizer colorizer = lexer.Colorize();
 			/// 
 			///     void* lastInterTokenState = nullptr;
-			///     FOREACH_INDEXER(const wchar_t*, line, index, From(lines))
+			///     for (auto [line, index] : indexed(From(lines)))
 			///     {
 			///         Console::WriteLine(L"Begin line " + itow(index));
 			///         argument.processingText = line;
@@ -617,7 +617,7 @@ Tokenizer
 		///     WString input = L"I have 2 books.";
 		///     auto tokenResult = lexer.Parse(input);
 		/// 
-		///     FOREACH(RegexToken, token, tokenResult)
+		///     for (auto token : tokenResult)
 		///     {
 		///         // input must be in a variable
 		///         // because token.reading points to a position from input.Buffer();
@@ -660,7 +660,7 @@ Tokenizer
 			///     List<RegexToken> filtered;
 			///     tokenResult.ReadToEnd(filtered, [](vint token) { return token < 0 || token == 2; });
 			/// 
-			///     FOREACH(RegexToken, token, tokenResult)
+			///     for (auto token : tokenResult)
 			///     {
 			///         // input must be in a variable
 			///         // because token.reading points to a position from input.Buffer();
@@ -817,7 +817,7 @@ Tokenizer
 			///     RegexLexerWalker walker = lexer.Walk();
 			/// 
 			///     WString tests[] = { L".", L"2", L"2.", L"2.5", L"2.5." };
-			///     FOREACH(WString, test, From(tests))
+			///     for (auto test : From(tests))
 			///     {
 			///         if (walker.IsClosedToken(test.Buffer(), test.Length()))
 			///         {
@@ -867,7 +867,7 @@ Tokenizer
 			///     RegexLexerWalker walker = lexer.Walk();
 			/// 
 			///     WString tests[] = { L".", L"2", L"2.", L"2.5", L"2.5." };
-			///     FOREACH(WString, test, From(tests))
+			///     for (auto test : From(tests))
 			///     {
 			///         if (walker.IsClosedToken(test))
 			///         {
@@ -926,7 +926,7 @@ Tokenizer
 		///     RegexLexer lexer(tokenDefs, proc);
 		///     RegexLexerColorizer colorizer = lexer.Colorize();
 		/// 
-		///     FOREACH_INDEXER(const wchar_t*, line, index, From(lines))
+		///     ///     for (auto [line, index] : indexed(From(lines)))
 		///     {
 		///         Console::WriteLine(L"Begin line " + itow(index));
 		///         argument.processingText = line;
